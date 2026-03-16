c280c8e312373e9d1191d50d27201372e5e89e9b
shivamjadhav <jadhavshivam0228@gmail.com>
2026-03-16 14:19:30 -0500

    updated: fixed user edit issue with password optional

--- frontend/src/components/modals/User.js
+++ frontend/src/components/modals/User.js
@@ -15,6 +15,7 @@ import IconButton from "@mui/material/IconButton";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import edit from "../../../../backend/src/routes/api/Common/images/edit";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -213,7 +214,7 @@ export default function UserModal({
             />
             <TextField
               id="password"
-              required
+              required = {edit ? false : true}
               value={formData.password}
               onChange={handleInputChange}
               name="password"
